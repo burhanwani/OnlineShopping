4fa4dbb0d3f60580f3dac0c177db1693ba036099
BURHAN AYUB <burhanayub@microsoft.com>
2022-02-09 21:27:36 -0800

    working shopping cart and adding to cart functionality. The names for DTOS need to be same in each microservice to work properly. Also be mindful of the nullable references in .net 6 enabled by default. Need to turn that off in the project setting.

--- Mango.Services.ShoppingCartAPI/Models/Dto/ProductDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/ProductDto.cs
@@ -10,9 +10,9 @@ namespace Mango.Services.ShoppingCartAPI.Models.Dto
     public class ProductDto
     {
         public int ProductId { get; set; }
-        public string Name { get; set; }
+        public string ProductName { get; set; }
         public double Price { get; set; }
-        public string Description { get; set; }
+        public string ProductDescription { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
     }
